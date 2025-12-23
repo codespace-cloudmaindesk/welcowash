f03c7c706ca198e3c0388ffc93f062d9764ac7aa
tudinkojhb024 <tudinkojhb024@student.wethinkcode.co.za>
2025-12-23 20:47:39 +0200

    refactored app.tsx to use barrel export on Nvbar for consistency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { JSX } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Component Imports
-import { Navbar } from "./components/layout/Navbar/Navbar";
+import { Navbar } from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import { GalleryPage } from "./pages/Gallery";
