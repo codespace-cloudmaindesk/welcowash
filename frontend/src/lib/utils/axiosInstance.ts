d5d78e45fbd4768001eb0c9f5ab04b877ee5f292
tudinkojhb024 <tudinkojhb024@student.wethinkcode.co.za>
2026-01-28 23:50:17 +0200

    feat: Add Axios instance utility with base URL and session token handling, and update .gitignore to ignore the .vs directory.

--- frontend/src/lib/utils/axiosInstance.ts
+++ frontend/src/lib/utils/axiosInstance.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const axiosInstance = () => {
   const token = typeof window !== "undefined" ? sessionStorage.getItem("token") : null;
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
   return axios.create({
-    baseURL: "/api",
+    baseURL: BASE_URL,
     headers: {
       "Content-Type": "application/json",
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
